7c326ecd99f434f8d65989a5b8ed438eb4430e4d
Dmytro Yefremenko <mitrandir.white@gmail.com>
2022-09-27 19:57:15 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,10 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
+const STORAGE_KEYS = 'feedback-form-state';
 const refs = {
     form: document.querySelector('.feedback-form'),
     email: document.querySelector('.feedback-form input[name=email]'),
-    message: document.querySelector('.feedback-form input[name=message]'),
+    message: document.querySelector('.feedback-form textarea'),
 };
 
 let formData = {};
@@ -17,20 +17,20 @@ populateFormData();
 function onFormSubmit(event) {
     event.preventDefault();
     event.target.reset();
-    localStorage.removeItem(STORAGE_KEY);
+    localStorage.removeItem(STORAGE_KEYS);
 };
 
 
 function onFormInput(event) {
     formData[event.target.name] = event.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    localStorage.setItem(STORAGE_KEYS, JSON.stringify(formData));
 };
 
 function populateFormData() {
-    const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (data) {
-        for (key in data) { 
-            refs[key].value = data[key];
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEYS));
+    if (savedData) {
+        for (const key in savedData) { 
+            refs[key].value = savedData[key];
         }
     };
 }
